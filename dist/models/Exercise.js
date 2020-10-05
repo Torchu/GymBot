a6a38f0e2584a3b39fe558d16a68ee26505a3abf
Víctor Cabrita Gómez <victor.cabritag@gmail.com>
2020-10-05 21:00:11 +0200

    Elimino carpeta dist

--- dist/models/Exercise.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-class Exercise {
-    constructor(name, muscles, description, weight) {
-        this.name = name;
-        this.muscles = muscles;
-        this.description = description;
-        this.weight = weight;
-    }
-}
