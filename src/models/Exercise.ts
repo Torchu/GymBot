74b61a779ebb6a1fa79a7dcf52131a7da4e29a92
Víctor Cabrita Gómez <victor.cabritag@gmail.com>
2020-10-05 20:02:33 +0200

    Creada entidad, closes #2

---
+++ src/models/Exercise.ts
@@ -0,0 +1,8 @@
+class Exercise { 
+    constructor(
+        public name: string,
+        public muscles: string[], 
+        public description: string,
+        public weight: number
+    ) {}
+}
