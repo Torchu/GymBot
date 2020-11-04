63f2ce20567abb437df95d2eba7e11ae6b88eeb8
Víctor Cabrita Gómez <victor.cabritag@gmail.com>
2020-11-04 17:30:18 +0100

    testing something of shippable, updates #17

--- src/models/exercise.ts
+++ src/models/exercise.ts
@@ -1,31 +1,31 @@
 //Clase sin métodos que define el objeto ejercicio
-class Exercise { 
-    private _weight: number;
-    
-    constructor(readonly name: string, _weight: number) {
-        if(_weight < 0){
+class Exercise {
+  private _weight: number;
+
+  constructor(readonly name: string, _weight: number) {
+    /*if(_weight < 0){
             throw new Error("Invalid weight: Weights can't be negative numbers");
-        }
-        this._weight = _weight;
-    }
+        }*/
+    this._weight = _weight;
+  }
 
-    get weight(): number{
-        return this._weight;
-    }
+  get weight(): number {
+    return this._weight;
+  }
 
-    set weight(newWeight: number){
-        if(newWeight < 0){
-            throw new Error("Invalid weight: Weights can't be negative numbers");
-        }
-        this._weight=newWeight;
+  set weight(newWeight: number) {
+    if (newWeight < 0) {
+      throw new Error("Invalid weight: Weights can't be negative numbers");
     }
+    this._weight = newWeight;
+  }
 
-    increaseWeight(increase: number){
-        if(increase < 0){
-            throw new Error("Increases must be positive");
-        }
-        this._weight += increase;
+  increaseWeight(increase: number) {
+    if (increase < 0) {
+      throw new Error("Increases must be positive");
     }
+    this._weight += increase;
+  }
 }
 
-export default Exercise
+export default Exercise;
