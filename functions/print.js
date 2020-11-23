eacf7a23f630fdd7d82cd95a3e707005f220e478
torchu <victor.cabritag@gmail.com>
2020-11-23 19:03:17 +0100

    fourth try

--- functions/print.js
+++ functions/print.js
@@ -1,30 +1,8 @@
-import Exercise from "../src/models/exercise";
-import Routine from "../src/models/routine";
-const test = new Routine(
-  [
-    new Exercise("Press Banca", 40),
-    new Exercise("Press Militar", 30),
-    new Exercise("Flexiones", 0),
-  ],
-  [],
-  [
-    new Exercise("Dominadas", 0),
-    new Exercise("Remo Barra", 30),
-    new Exercise("Jalón al pecho", 40),
-  ],
-  [],
-  [
-    new Exercise("Sentadilla", 80),
-    new Exercise("Peso Muerto", 70),
-    new Exercise("Abdominales", 0),
-  ],
-  [],
-  []
-);
+const data = require("./example.json");
 
 exports.handler = function (event, context, callback) {
   callback(null, {
     statusCode: 200,
-    body: JSON.stringify(test),
+    body: JSON.stringify(data),
   });
 };
