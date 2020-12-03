e548f0ce11e500e40545b047c5d0127ddbd6c12d
torchu <victor.cabritag@gmail.com>
2020-12-03 21:05:31 +0100

    print fixed

--- functions/print.js
+++ functions/print.js
@@ -3,6 +3,6 @@ const data = require("./today.json");
 exports.handler = function (event, context, callback) {
   callback(null, {
     statusCode: 200,
-    body: JSON.stringify(data),
+    body: data,
   });
 };
