9460a720b7e8bfd7c34a7da8b9229164d52be232
torchu <victor.cabritag@gmail.com>
2020-12-14 20:36:41 +0100

    tests fixed

--- src/__tests__/app.test.ts
+++ src/__tests__/app.test.ts
@@ -1,20 +1,20 @@
 const request = require("supertest");
-const app = require("../app");
+const apps = require("../app");
 
 describe("Routes tests", () => {
   it("should greet you", async () => {
-    const response = await request(app).get("/");
+    const response = await request(apps).get("/");
     expect(response.status).toBe(200);
     expect(response.body.message).toEqual("Welcome to GymBot");
   });
 
   it("should give you the routine", async (done) => {
     const result = require("../../db/routine.json");
-    request(app).get("/routine").expect(200).expect(result, done);
+    request(apps).get("/routine").expect(200).expect(result, done);
   });
 
   it("should modify the routine", async (done) => {
-    request(app)
+    request(apps)
       .put("/routine")
       .send({
         monday: [
@@ -38,17 +38,17 @@ describe("Routes tests", () => {
   });
 
   it("should clean the routine", async (done) => {
-    request(app)
+    request(apps)
       .delete("/routine")
       .expect(200)
       .expect('"La rutina ha sido borrada"', done);
   });
 
   it("should give today's work", async () => {
-    request(app).get("/print").expect(200);
+    request(apps).get("/print").expect(200);
   });
 
   afterAll(async () => {
-    await app.close();
+    await apps.close();
   });
 });
--- src/server.ts
+++
@@ -1,6 +0,0 @@
-const application = require('./app');
-const port = process.env.port || 8080;
-
-application.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
-  });
