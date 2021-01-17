f26f0cceb2d7d1332bd7ca0e96f822db79bd5f6c
torchu <victor.cabritag@gmail.com>
2021-01-17 21:45:41 +0100

    env vars

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,12 @@
 import app from "./app";
 import mongoose from "mongoose";
 const port = process.env.PORT || 8080;
+//mongodb+srv://Heroku:herokuapp1234@cluster0.mu2oo.mongodb.net/gymbot?retryWrites=true&w=majority
 
-mongoose.connect(
-  "mongodb+srv://Heroku:herokuapp1234@cluster0.mu2oo.mongodb.net/<dbname>?retryWrites=true&w=majority",
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  }
-);
+mongoose.connect(process.env.DATABASE_URL!, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 const db = mongoose.connection;
 db.on("error", (err) => console.log(err));
 db.once("open", () => console.log("Connected to DB"));
