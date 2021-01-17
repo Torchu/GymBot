4b9d6bc1ff02cec11e07327b5f10be986a165a48
torchu <victor.cabritag@gmail.com>
2021-01-17 21:35:23 +0100

    atlas

--- src/server.ts
+++ src/server.ts
@@ -2,14 +2,16 @@ import app from "./app";
 import mongoose from "mongoose";
 const port = process.env.PORT || 8080;
 
-/*
-mongoose.connect("mongodb://localhost/gymbot", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose.connect(
+  "mongodb+srv://Heroku:herokuapp1234@cluster0.mu2oo.mongodb.net/<dbname>?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
 const db = mongoose.connection;
 db.on("error", (err) => console.log(err));
-db.once("open", () => console.log("Connected to DB"));*/
+db.once("open", () => console.log("Connected to DB"));
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
