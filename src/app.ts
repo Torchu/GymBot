8c299f47804fdf19a7a1d7e4ee721e4ebe239a2b
torchu <victor.cabritag@gmail.com>
2021-01-17 12:50:15 +0100

    morgan logger

--- src/app.ts
+++ src/app.ts
@@ -3,11 +3,13 @@ import { routineParser } from "./middleware/routine.parser";
 
 import express from "express";
 import bodyParser from "body-parser";
+import morgan from "morgan";
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(morgan("dev"));
 
 let routine = new Routine([], [], [], [], [], [], []);
 
